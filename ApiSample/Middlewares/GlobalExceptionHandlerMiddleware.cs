8f9aa5140aa30fc84487ec2a9c0b70e152c1d518
Thinh Truong <thinh2it@gmail.com>
2023-08-27 20:35:01 +0700

    Add global exception handler

---
+++ ApiSample/Middlewares/GlobalExceptionHandlerMiddleware.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Text.Json;
+
+namespace ApiSample.Middlewares
+{
+    public class GlobalExceptionHandlerMiddleware : IMiddleware
+    {
+        private readonly ILogger<GlobalExceptionHandlerMiddleware> _logger;
+
+        public GlobalExceptionHandlerMiddleware(ILogger<GlobalExceptionHandlerMiddleware> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, e.Message);
+
+                context.Response.StatusCode =
+                    (int)HttpStatusCode.InternalServerError;
+
+                ProblemDetails problem = new()
+                {
+                    Status = (int)HttpStatusCode.InternalServerError,
+                    Type = "Server error",
+                    Title = "Server error",
+                    Detail = "An internal server has occured"
+                };
+
+                string json = JsonSerializer.Serialize(problem);
+
+                context.Response.ContentType = "application/json";
+
+                await context.Response.WriteAsync(json);
+            }
+        }
+    }
+}
