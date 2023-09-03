43381d3be6fb74156dbd85582284e2ae058d2998
Thinh Truong <thinh.truong@smartaction.com>
2023-09-03 22:33:15 +0700

    Add TenantProvider class

---
+++ ApiSample/TenantProvider.cs
@@ -0,0 +1,18 @@
+﻿namespace MCR.App
+{
+    public sealed class TenantProvider
+    {
+        private const string TenantIdHeaderName = "X-TenantId";
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public TenantProvider(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string TenantId => _httpContextAccessor
+            .HttpContext
+            .Request
+            .Headers[TenantIdHeaderName];
+    }
+}
